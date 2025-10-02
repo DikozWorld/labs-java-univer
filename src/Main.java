a71f5c86e8d59e6cc79df5f0fb3692df99d46e72
DikozWorld <dikoz389@gmail.com>
2025-10-02 21:38:56 +0300

    тест

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         analyzeText(text);
     }
-
+//тест
     public static void analyzeText(String text) {
         StringReader reader = new StringReader(text);
         StreamTokenizer tokenizer = new StreamTokenizer(reader);
